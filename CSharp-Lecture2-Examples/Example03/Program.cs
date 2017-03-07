3088d8f251c54e25a2a079bb8601a479f45319e5
due2401 <hibik610@gmail.com>
2017-03-07 20:46:19 +0800

    Example3: 加入string用法

---
+++ CSharp-Lecture2-Examples/Example03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example03
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string sql =
+                @"select * from table
+                  where id > 1000";
+            string xml =
+                @"<Pokemon>
+                    <NationalNo>#001</National>
+                    <Name>Bulbsaur</Name>
+                  </Pokemon>";
+            string json =
+                @"Pokemon = {
+                    NationalN: '#001',
+                    Name:Bulbsaur
+                  }";
+
+            Console.WriteLine(sql);
+            Console.WriteLine(xml);
+            Console.WriteLine(json);
+        }
+    }
+}
